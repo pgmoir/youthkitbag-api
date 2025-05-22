dac25e44598b5cc749154ce09b9317fcb495465b
Philip Moir <phil@captur.ai>
2025-05-22 18:38:37 +0100

    Switch to fastify instead of express

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   app.enableCors();
   app.setGlobalPrefix('api');

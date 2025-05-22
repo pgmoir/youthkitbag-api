ff1e5492084b2e2b79f6e2b1c352164cd2daa3a2
Philip Moir <phil@captur.ai>
2025-05-22 20:10:16 +0100

    Re-add process env port for render deploy

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,11 +13,19 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('Youthkitbag API')
+    .setDescription('Authenticated backend for Youthkitbag Application')
+    .setVersion('1.0.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.enableCors();
   app.setGlobalPrefix('api');
 
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') ?? 3000;
+  const port = (process.env.PORT || configService.get<number>('PORT')) ?? 3000;
 
   await app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
